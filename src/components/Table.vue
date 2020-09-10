06ce467125e74cd9e96d4e34046ab5ad8c877cf4
Mochamad Sofyan <58968418+sofyanmoch@users.noreply.github.com>
2020-09-09 19:44:38 -0700

    Add files via upload

---
+++ src/components/Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { invoice: 40, orders: 'Dickerson', amount: 'Macdonald' },
+        { invoice: 21, orders: 'Larsen', amount: 'Shaw' },
+        { invoice: 89, orders: 'Geneva', amount: 'Wilson' },
+        { invoice: 38, orders: 'Jami', amount: 'Carney' }
+      ]
+    }
+  }
+}
+</script>
